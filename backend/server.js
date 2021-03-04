baa81c96320d7facc0f84de0bf9dec06655e4716
Ricardo Golac <rigoba98@hotmail.com>
2021-03-04 18:25:55 -0500

    Added some heroku scripts

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 // Routes
 const items = require('./routes/api/items')
@@ -9,27 +10,28 @@ const app = express();
 app.use(express.json());
 
 // Load Environment Variables
-const path = require('path');
 require('dotenv').config({path: path.resolve(__dirname, '../.env')});
 const port = process.env.PORT || 5000;
-
 // DB Config
 const db = process.env.MONGO_URI;
-
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('../frontend/build'));
-}
 
 // Connect to Mongo
 mongoose.connect(db, {useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-app.get('*', (request, response) => {
-  response.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
+
 
 // Use Routes
 app.use('/api/items', items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+	app.use(express.static('../frontend/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '/../frontend/build/index.html'))
+  });
+}
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
