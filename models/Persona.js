36d09117859c8e4f297cd5ac33c06f3b669c342c
Abinai Pasunuri <abhinay.pasunuri088@gmail.com>
2021-03-10 17:10:33 -0500

    Added some features to schema

--- models/Persona.js
+++ models/Persona.js
@@ -2,9 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PersonaSchema = new Schema({
-    name: {
+    major: {
         type: String,
         required: true
+    },
+    minor: {
+        type: String,
+        default: "N/A",
+        required: true
+    },
+    gpa: {
+        type: Number,
+        max: 4.0,
+        min: 0.0
     },
     age: {
         type: Number,
