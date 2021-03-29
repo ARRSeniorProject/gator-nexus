2f51b33d5fa5d86cd838738cf5e3121bf5b9f301
Abinai Pasunuri <abhinay.pasunuri088@gmail.com>
2021-03-29 17:38:00 -0400

    added Aggregate endpoint

--- routes/api/aggregate.js
+++ routes/api/aggregate.js
@@ -9,8 +9,32 @@ router.get('/', (req, res) => {
         }
         else {
             let aggregatePersona = {};
+            let academicStandingCount = {
+                'Freshman' : 0,
+                'Sophomore' : 0,
+                'Junior' : 0,
+                'Senior' : 0,
+                'Super Senior' : 0
+            };
+            let raceCount = {
+                'White' : 0, 
+                'Black or African American' : 0, 
+                'Hispanic or Latino' : 0, 
+                'American Indian or Alaska Native' : 0, 
+                'Asian' : 0, 
+                'Native Hawaiian or Other Pacific Islander' : 0
+            };
+            let genderCount = {};
             let averageGPA = personas.reduce((total, persona) => total + persona.gpa, 0) / personas.length;
+            let averageAge = personas.reduce((total, persona) => total + persona.age, 0) / personas.length;
+            let averageIncome = personas.reduce((total, persona) => total + persona.householdIncome, 0) / personas.length;
+            let averageEmploymentStatus = personas.reduce((total, persona) => total + persona.employmentStatus, 0) / personas.length;
+            let averageInterviewPreparationTime = personas.reduce((total, persona) => total + persona.interviewPreparationTime, 0) / personas.length;
             aggregatePersona['gpa'] = averageGPA;
+            aggregatePersona['age'] = averageAge;
+            aggregatePersona['householdIncome'] = averageIncome;
+            aggregatePersona['employmentStatus'] = averageEmploymentStatus;
+            aggregatePersona['interviewPreparationTime'] = averageInterviewPreparationTime;
             res.json(aggregatePersona);
         }
     });
