a3b9fd6d6af573716de2faf4028c951cb11314ef
Abinai Pasunuri <abhinay.pasunuri088@gmail.com>
2021-04-05 12:44:09 -0400

    Profile Page formatting changes

--- frontend/src/components/NewProfileForm.js
+++ frontend/src/components/NewProfileForm.js
@@ -1,6 +1,6 @@
 import React, { Component, useState } from 'react';
+import { Button, Form, FormGroup, Label, Input} from 'reactstrap';
 import axios from 'axios';
-import $ from 'jquery';
 //import Card from '@material-ui/core/Card';
 //import CardActionArea from '@material-ui/core/CardActionArea';
 //import CardActions from '@material-ui/core/CardActions';
@@ -16,35 +16,21 @@ class NewProfileForm extends Component {
         this.state = {
             gender: '',
             race: '',
-            age: 0,
-            householdIncome: 0,
-            employmentStatus: 0,
+            age: '',
+            householdIncome: '',
+            employmentStatus: '',
             phoneNumber: '',
             email: '',
             company: '',
-            academicStanding: 0,
+            academicStanding: '',
             major: '',
             minor: '',
-            gpa: 0,
-            interviewPreparationTime: 0,
+            gpa: '',
+            interviewPreparationTime: '',
             skills: [],
             profilePicture: null
         }
-    }
-
-    ocShowAlert = (message, background = '#3089cf') => {
-        let alertContainer = document.querySelector('#oc-alert-container');
-        let alertEl = document.createElement('div');
-        let textNode = document.createTextNode(message);
-        alertEl.setAttribute('class', 'oc-alert-pop-up');
-        $(alertEl).css('background', background);
-        alertEl.appendChild(textNode);
-        alertContainer.appendChild(alertEl);
-        setTimeout(() => {
-            $(alertEl).fadeOut('slow');
-            $(alertEl).remove();
-        }, 3000);
-    };    
+    } 
 
     fileChangedHandler = (event) => {
         this.setState({
@@ -54,26 +40,33 @@ class NewProfileForm extends Component {
 
     onSubmit = (event) => {
         event.preventDefault();
-        console.log(this.state);
+        var newProfile = {
+            gender: this.state.gender,
+            race: this.state.race,
+            age: parseInt(this.state.age),
+            householdIncome: parseInt(this.state.householdIncome),
+            employmentStatus: parseInt(this.state.employmentStatus),
+            academicStanding: parseInt(this.state.academicStanding),
+            major: this.state.major,
+            gpa: parseFloat(this.state.gpa),
+            interviewPreparationTime: parseInt(this.state.interviewPreparationTime),
+            skills: ['Python', 'Java', 'C++']
+        }
+        if(this.state.company != "") {
+            newProfile.company = this.state.company;
+        }
+        if(this.state.minor != "") {
+            newProfile.minor = this.state.minor;
+        }
+        if(this.state.phoneNumber != "") {
+            newProfile.phoneNumber = this.state.phoneNumber;
+        }
+        if(this.state.email != "") {
+            newProfile.email = this.state.email;
+        }
         const data = new FormData();
         if(this.state.profilePicture) {
             data.append('profilePicture', this.state.profilePicture, this.state.profilePicture.name);
-            var newProfile = {
-                gender: this.state.gender,
-                race: this.state.race,
-                age: this.state.age,
-                householdIncome: this.state.householdIncome,
-                employmentStatus: this.state.employmentStatus,
-                academicStanding: this.state.academicStanding,
-                major: this.state.major,
-                gpa: this.state.gpa,
-                interviewPreparationTime: this.state.interviewPreparationTime,
-                skills: ['Python', 'Java', 'C++']
-            }
-            var company = this.state.company;
-            var minor = this.state.minor;
-            var phoneNumber = this.state.phoneNumber;
-            var email = this.state.email;
             axios.post('/api/profile-picture/upload', data, {
                 headers: {
                     'accept': 'application/json',
@@ -93,19 +86,9 @@ class NewProfileForm extends Component {
                     } 
                     else {
                         console.log('File Uploaded');
-                        console.log(res);
-                        if(company != "") {
-                            newProfile.company = company;
-                        }
-                        if(minor != "") {
-                            newProfile.minor = minor;
-                        }
-                        if(phoneNumber != "") {
-                            newProfile.phoneNumber = phoneNumber;
-                        }
-                        if(email != "") {
-                            newProfile.email = email;
-                        }
+                        console.log(res.data.location);
+                        newProfile.profilePicture = res.data.location;
+                        console.log(newProfile);
                         axios.post('/api/personas', newProfile).then(res => console.log(res.data));
                     }
                 }
@@ -114,22 +97,23 @@ class NewProfileForm extends Component {
             });
         } 
         else {
-            console.log('Please upload file');
+            console.log(newProfile);
+            axios.post('/api/personas', newProfile).then(res => console.log(res.data));
         }
         this.setState({
             gender: '',
             race: '',
-            age: 0,
-            householdIncome: 0,
-            employmentStatus: 0,
+            age: '',
+            householdIncome: '',
+            employmentStatus: '',
             phoneNumber: '',
             email: '',
             company: '',
-            academicStanding: 0,
+            academicStanding: '',
             major: '',
             minor: '',
-            gpa: 0,
-            interviewPreparationTime: 0,
+            gpa: '',
+            interviewPreparationTime: '',
             skills: [],
             profilePicture: null
         });
@@ -143,104 +127,106 @@ class NewProfileForm extends Component {
 
     render() {
         return(
-            <div>
-                <h1>Enter in your Information</h1>
-                <form>
-                    <h3>Personal Information</h3>
-                    <select name="race" onChange={e => this.change(e)}>
-                        <option value="">---</option>
-                        <option value="White">White</option>
-                        <option value="Black">Black</option>
-                        <option value="Hispanic or Latino">Hispanic or Latino</option>
-                        <option value="Native American">Native American</option>
-                        <option value="Asian">Asian</option>
-                        <option value="Pacific Islander">Pacific Islander</option>
-                        <option value="Other">Other</option>
-                    </select>
-                    <select name="gender" onChange={e => this.change(e)}>
-                        <option value="">---</option>
-                        <option value="Male">Male</option>
-                        <option value="Female">Female</option>
-                        <option value="Other">Other</option>
-                    </select>
-                    <br />
-                    <input 
-                    name="age"
-                    placeholder='Age'
-                    value={this.state.age}
-                    onChange={e => this.change(e)} 
-                    />
-                    <input 
-                    name="householdIncome"
-                    placeholder='Household Income'
-                    value={this.state.householdIncome}
-                    onChange={e => this.change(e)} 
-                    />
-                    <input 
-                    name="employmentStatus"
-                    placeholder='Employment Status'
-                    value={this.state.employmentStatus}
-                    onChange={e => this.change(e)} 
-                    />
-                    <br />
-                    <input 
-                    name="phoneNumber"
-                    placeholder='Phone Number'
-                    value={this.state.phoneNumber}
-                    onChange={e => this.change(e)} 
-                    />
-                    <input 
-                    name="email"
-                    placeholder='Email'
-                    value={this.state.email}
-                    onChange={e => this.change(e)} 
-                    />
-                    <br />
-                    <input 
-                    type="file"
-                    onChange={this.fileChangedHandler} 
-                    />
-                    <h3>Professional Information</h3>
-                    <input 
-                    name="company"
-                    placeholder='Company'
-                    value={this.state.company}
-                    onChange={e => this.change(e)} 
-                    />
-                    <br />
-                    <input 
-                    name="academicStanding"
-                    placeholder='Academic Standing'
-                    value={this.state.academicStanding}
-                    onChange={e => this.change(e)} 
-                    />
-                    <input 
-                    name="major"
-                    placeholder='Major'
-                    value={this.state.major}
-                    onChange={e => this.change(e)} 
-                    />
-                    <input 
-                    name="minor"
-                    placeholder='Minor'
-                    value={this.state.minor}
-                    onChange={e => this.change(e)} 
-                    />
-                    <input 
-                    name="gpa"
-                    placeholder='GPA'
-                    value={this.state.gpa}
-                    onChange={e => this.change(e)} 
-                    />
-                    <input 
-                    name="interviewPreparationTime"
-                    placeholder='Interview Preparation Time'
-                    value={this.state.interviewPreparationTime}
-                    onChange={e => this.change(e)} 
-                    />
-                    <br />
-                    <button onClick={e => this.onSubmit(e)}>Submit</button>
-                </form>
+            <div className="form-wrapper">
+                <center><h1>Enter in your Information</h1></center>
+                <Form>
+                    <FormGroup>
+                        <h3>Personal Information</h3>
+                        <Input type="select" name="race" onChange={e => this.change(e)}>
+                            <option value="">---</option>
+                            <option value="White">White</option>
+                            <option value="Black">Black</option>
+                            <option value="Hispanic or Latino">Hispanic or Latino</option>
+                            <option value="Native American">Native American</option>
+                            <option value="Asian">Asian</option>
+                            <option value="Pacific Islander">Pacific Islander</option>
+                            <option value="Other">Other</option>
+                        </Input>
+                        <Input type="select" name="gender" onChange={e => this.change(e)}>
+                            <option value="">---</option>
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                            <option value="Other">Other</option>
+                        </Input>
+                        <br />
+                        <Input 
+                        name="age"
+                        placeholder='Age'
+                        value={this.state.age}
+                        onChange={e => this.change(e)} 
+                        />
+                        <Input 
+                        name="householdIncome"
+                        placeholder='Household Income'
+                        value={this.state.householdIncome}
+                        onChange={e => this.change(e)} 
+                        />
+                        <Input 
+                        name="employmentStatus"
+                        placeholder='Employment Status'
+                        value={this.state.employmentStatus}
+                        onChange={e => this.change(e)} 
+                        />
+                        <br />
+                        <Input 
+                        name="phoneNumber"
+                        placeholder='Phone Number'
+                        value={this.state.phoneNumber}
+                        onChange={e => this.change(e)} 
+                        />
+                        <Input 
+                        name="email"
+                        placeholder='Email'
+                        value={this.state.email}
+                        onChange={e => this.change(e)} 
+                        />
+                        <br />
+                        <Input 
+                        type="file"
+                        onChange={this.fileChangedHandler} 
+                        />
+                        <h3>Professional Information</h3>
+                        <Input 
+                        name="academicStanding"
+                        placeholder='Academic Standing'
+                        value={this.state.academicStanding}
+                        onChange={e => this.change(e)} 
+                        />
+                        <Input 
+                        name="major"
+                        placeholder='Major'
+                        value={this.state.major}
+                        onChange={e => this.change(e)} 
+                        />
+                        <Input 
+                        name="minor"
+                        placeholder='Minor'
+                        value={this.state.minor}
+                        onChange={e => this.change(e)} 
+                        />
+                        <Input 
+                        name="gpa"
+                        placeholder='GPA'
+                        value={this.state.gpa}
+                        onChange={e => this.change(e)} 
+                        />
+                        <br />
+                        <Input 
+                        name="company"
+                        placeholder='Company'
+                        value={this.state.company}
+                        onChange={e => this.change(e)} 
+                        />
+                        <Input 
+                        name="interviewPreparationTime"
+                        placeholder='Interview Preparation Time'
+                        value={this.state.interviewPreparationTime}
+                        onChange={e => this.change(e)} 
+                        />
+                        <br />
+                        <Button onClick={e => this.onSubmit(e)}>Submit</Button>
+                    </FormGroup>
+                </Form>
             </div>
         );
     }
